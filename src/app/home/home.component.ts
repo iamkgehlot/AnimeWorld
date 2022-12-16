25f1618598d6d98f2c65ad2561589cc32c16ff09
iamkgehlot <iamkgehlot@gmail.com>
2022-12-17 00:43:28 +0530

    perf + best practices

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { SearchResultServiceService } from '../services/search-result-service.service';
 
-import *  as $ from "jquery";
 
 
 @Component({
