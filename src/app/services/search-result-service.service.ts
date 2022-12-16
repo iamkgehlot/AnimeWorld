7db4bee5e5c0efdc04d05dc820511c28301d7dd0
iamkgehlot <iamkgehlot@gmail.com>
2022-12-17 03:39:41 +0530

    Overall Improvement check 1

--- src/app/services/search-result-service.service.ts
+++ src/app/services/search-result-service.service.ts
@@ -20,12 +20,12 @@ export class SearchResultServiceService {
 
   //next prev pge
   getTop(pagenum:any): Observable<any> {
-    return this.http.get('https://api.jikan.moe/v4/top/anime?limit=5');
+    return this.http.get('https://api.jikan.moe/v4/top/anime?page='+pagenum+'&limit=20&sfw');
     
   }
   //popular anime
   getTopUpcoming(): Observable<any> {
-    return this.http.get('https://api.jikan.moe/v4/top/anime?filter=bypopularity&limit=4&sfw&type=tv');
+    return this.http.get('https://api.jikan.moe/v4/top/anime?filter=bypopularity&limit=4&sfw');
     
   }
   //popular movie
